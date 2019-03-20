d89e6d85a9b295db17676cfd4e7b3c33001fd557
Priyankaz <mailpriyanka321@gmail.com>
2019-03-20 13:05:51 +0530

    Intial Test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.btnId);
+
+    }
+}
